b7a2727f7d6a99106b20b50f8d46bf1d7192eae8
EC2 Default User <ec2-user@ip-172-31-7-41.ap-northeast-1.compute.internal>
2023-02-07 12:52:19 +0000

    [Finish]非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,13 +6,18 @@ class BookCommentsController < ApplicationController
     # comment = BookComment.new(book_comment_params)
     # comment.user_id = current_user.id
     @comment.book_id = @book.id
-    @comment.save
-    redirect_to book_path(@book)
+    # create.js.erbで指定している為、book_controllerでも指定する必要がある。
+    @book_comment = BookComment.new
+    unless @comment.save
+      render 'error'
+    end
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    # destroy.js.erbで指定している為、book_controllerでも指定する必要がある。
+    @book_comment = BookComment.new
   end
 
 
